eb8cc0a3b5341328b14c2621d983106f9cb8e7f4
Alan <alan345@gmail.com>
2017-03-30 10:43:39 -0700

    change database info

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   //'database': 'mongodb://localhost:27017/ng2_Form',
-  'database': 'mongodb://alan345:alan345@ds153677.mlab.com:53677/heroku_6f0h7zgr/vulgar-test', //'mongodb://localhost:27017/ng2_Form',
+  'database': 'mongodb://gooplus:LrrpmFpiVe4lVJAS1SO3Q06cR0aWa2zAfM4MkOiDZqVt9UkN97lQze2kfTebQMKJGBEnEgS1JksWrNQaB0X5C8gmBL2Lwv0E@ds145800.mlab.com:45800/saloon', //'mongodb://localhost:27017/ng2_Form',
   'secret': 'SUPERsecret', // change this to a hard to guess random string. it's for jwt encryption and decryption
   'api_user': 'YOUR SENDGRID USERNAME',
   'api_key': 'YOUR SENDGRID PASSWORD',
