56a0f4d532702319ff85a1fc3a680e1a9812b851
alan345 <alan345@gmail.com>
2017-04-24 12:36:29 -0700

    smtp

--- server/routes/forgetPassword.js
+++ server/routes/forgetPassword.js
@@ -8,6 +8,7 @@ var express     = require('express'),
 
 var User = require('../models/user.model');
 
+
 // requesting password reset and setting the fields resetPasswordToken to a newly generated token
 // and resetPasswordExpires to the exact date the form is submitted so we can set/check the validity of the timestamp (token is valid for only one hour)
 // after that, the user must request a new password reset.
