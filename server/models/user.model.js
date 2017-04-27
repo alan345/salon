19ae79cc6ebb6e123aa6548b5aca0477837319ec
alan345 <alan345@gmail.com>
2017-04-27 09:27:37 -0700

    Solved issue with array picture

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -21,7 +21,7 @@ var user = new Schema({
     // you might want more user roles, so an array would be fine
     role: {type: Array, default: ['user']},
     profile : {
-      _profilePicture : {type: Schema.Types.ObjectId, ref: 'Form'},
+      _profilePicture : [{type: Schema.Types.ObjectId, ref: 'Form'}],
       parentUser: [{type: Schema.Types.ObjectId, ref: 'User'}],
       name: {type: String, default: ['']},
       title: {type: String, default: ['']},
