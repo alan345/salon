8e7c13a61a9a6058e266410399cae9a2eff47985
alan345 <alan345@gmail.com>
2017-04-27 17:50:46 -0700

    ok

--- src/app/press/press.model.ts
+++ src/app/press/press.model.ts
@@ -7,25 +7,25 @@ export interface Press {
   owner:{
     _id: string;
   };
-  formPDF:{
+  formPDF:[{
     _id: string;
     owner: string;
     imagePath: string;
-  };
-  form:{
+  }];
+  form:[{
     _id: string;
     owner: string;
     imagePath: string;
-  };
+  }];
   // date: {
   //   dateBegin:Date;
   //   dateEnd:Date;
   // }
 }
 //
-// export interface FormPDF {
-//   _id: string;
-//   owner: string;
-//   imagePath: string;
-//
-// }
+export interface FormPDF {
+  _id: string;
+  owner: string;
+  imagePath: string;
+
+}
