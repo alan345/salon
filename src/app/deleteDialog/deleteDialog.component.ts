780f8475c3eb42b697e66a0f3750ea830de203fd
alan345 <alan345@gmail.com>
2017-05-01 11:21:56 -0700

    ok

--- src/app/deleteDialog/deleteDialog.component.ts
+++ src/app/deleteDialog/deleteDialog.component.ts
@@ -14,6 +14,7 @@ import { FormBuilder, FormGroup, FormArray, FormControl, Validators} from '@angu
 import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser'
 
 
+
 @Component({
   selector: 'pressDeleteDialog',
   templateUrl: './deleteDialog.component.html',
