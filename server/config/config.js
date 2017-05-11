be069430e474e6e8ea509e7a5612d863353a8d0e
alan345 <alan345@gmail.com>
2017-05-11 16:49:33 -0700

    change database

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,8 @@ module.exports = {
   //'database': 'mongodb://localhost:27017/ng2_Form',
 
   // mongo ds145790.mlab.com:45790/saloon -u gooplus -p nkasndasdTY54TGa
-  'database': 'mongodb://gooplus:nkasndasdTY54TGa@ds145790.mlab.com:45790/gooplus', //'mongodb://localhost:27017/ng2_Form',
+  //'database': 'mongodb://gooplus:nkasndasdTY54TGa@ds145790.mlab.com:45790/gooplus',
+  'database': 'mongodb://localhost:27017/gooplus',
   'secret': 'SUPERsecret', // change this to a hard to guess random string. it's for jwt encryption and decryption
   'api_user': 'YOUR SENDGRID USERNAME',
   'api_key': 'YOUR SENDGRID PASSWORD',
