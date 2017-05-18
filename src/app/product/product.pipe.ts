2906caa0545a84bd08476738e540629a44366808
alan345 <alan345@gmail.com>
2017-05-17 17:55:40 -0700

    Clean Pipe

--- src/app/product/product.pipe.ts
+++ src/app/product/product.pipe.ts
@@ -1,18 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
-/*
- * Raise the value exponentially
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength:exponent
- * Example:
- *   {{ 2 |  exponentialStrength:10}}
- *   formats to: 1024
-*/
+
 @Pipe({name: 'smallDescription'})
 export class SmallDescriptionPipe implements PipeTransform {
   transform(description: string): string {
     //description = description.replace(/<\/?[^>]+(>|$)/g, "");
-    //return Math.pow(value, isNaN(exp) ? 1 : exp);
     return description.substring(0,90) + '...'
   }
 }
