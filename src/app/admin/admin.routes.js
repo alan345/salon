56fba4c8fa89942d696954b93b02bf3c91eb6daf
alan345 <alan345@gmail.com>
2017-05-26 12:39:33 -0700

    remove  all .map

--- src/app/admin/admin.routes.js
+++
@@ -1,6 +0,0 @@
-import { AdminGuardService } from './services/adminGuard';
-import { AdminUsersComponent } from './user/adminUsers.component';
-export var ADMIN_ROUTES = [
-    { path: 'user', component: AdminUsersComponent, canActivate: [AdminGuardService] },
-];
-//# sourceMappingURL=admin.routes.js.map
